d1d702c234d819b75b83f233379bf7492fe440cd
Strat-Sint <stratsint94@gmail.com>
2025-03-18 16:33:55 +0200

    Update NotFnd.jsx

--- recipe/src/components/NotFnd.jsx
+++ recipe/src/components/NotFnd.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import NavBar from './NavBar'
 
-function NotFnd( {number} ) {
+function NotFnd(  ) {
   return (
     <div>
         <NavBar />
       <div className="notFound">
         <h1>404 Page Not Found</h1>
         <p>Sorry, the recipe you are looking for doesn't exist.</p>
-        <p>We currently have {number} different recipes.</p>
-        <p>Please enter a value of 1 up to {number}</p>
+        <p>We currently have 30 different recipes.</p>
+        <p>Please enter a value of 1 up to 30</p>
       </div>
     </div>
   )
